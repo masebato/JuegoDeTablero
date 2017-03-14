b845905b13a7dcfe3281d95d5e34ff79c2b9bea8
masebato <baradona69@gmail.com>
2017-03-14 14:25:26 -0500

    Tabla funcionando

--- Index.js
+++ Index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
 
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
@@ -13,8 +14,6 @@ io.on('connection', function (socket) {
     /*socket.on('disconnect', function () {
         console.log('Usuario desconectado');
     });*/
-
-
 
     //establecer las variables para la conexion
     var connection = mysql.createConnection({
@@ -28,7 +27,7 @@ io.on('connection', function (socket) {
 
     //conectar a la base de datos
     connection.connect(function (error) {
-        console.log('asd');
+
         if (error) {
             throw error;
         } else {
@@ -36,36 +35,29 @@ io.on('connection', function (socket) {
         }
     });
     // hacer la consulta 
-    socket.emit('Cargar Datos', function (resultado) {
-
-        var query = connection.query('SELECT * from tablero;', [2], function (error, result) {
-            if (error) {
-                throw error;
-            } else {
-                var resultado = result;
-                 separador = "";
-                if (resultado.length > 0) {
-                    for (i = 0; i<resultado.length ; i++) {
-
-                        valores = resultado[i].C1 +''+ resultado[i].C2 +''+ resultado[i].C3 + '' + resultado[i].C4 + '' + resultado[i].C5 + '' + resultado[i].C6 + '' + resultado[i].C7 + '' + resultado[i].C8 + '' + resultado[i].C9 + '' + resultado[i].C10;
-                        io.emit('Cargar Datos',valores);
+    function EnviarDatos() {
+        socket.emit('Cargar Datos', function (resultado) {
+            var query = connection.query('SELECT * from tablero;', [], function (error, result) {
+                if (error) {
+                    throw error;
+                } else {
+                    var resultado = result;
+                    if (resultado.length > 0) {
+                        io.emit('Cargar Datos', result);
+                        //  io.emit('Cargar Datos', valores.split(separador));
+                    } else {
+                        console.log('Registro no encontrado');
 
                     }
-                   
-                  //  io.emit('Cargar Datos', valores.split(separador));
-
-                } else {
-                    console.log('Registro no encontrado');
                 }
-            }
-
-
+            });
         });
+    };
+    EnviarDatos();
 
-
-    });
-
-
+    setInterval(() => {
+        EnviarDatos();
+    }, 1000);
 });
 
 

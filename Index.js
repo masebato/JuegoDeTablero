81ab75b1fe1a2901133eecde532145e6d1bda058
masebato <baradona69@gmail.com>
2017-03-17 21:06:19 -0500

    Funciona el generar obstaculos

--- Index.js
+++ Index.js
@@ -34,6 +34,36 @@ io.on('connection', function (socket) {
             console.log('Conexion Correcta');
         }
     });
+
+    function GenerarObstaculos() {
+
+        var cont = 0;
+        var cont2 = Math.floor(Math.random() * (4 - 1) + 1);
+
+        for (con2 = 3; con2 < 9; con2++) {
+     for (var index = 1; index < 4; index++) {
+        
+          var query = connection.query('update tablero set C' + con2 + '=0 where IdFilas=' + index + ' and C' + con2 + ' IN(3);');
+     }
+           
+        }
+
+       for (con2 = 3; con2 < 9; con2++) {
+
+            cont2 = Math.floor(Math.random() * (4 - 1) + 1);
+            while (cont == cont2) {
+                cont2 = Math.floor(Math.random() * (3 - 1) + 1);
+            }
+            var query = connection.query('update tablero set C' + con2 + '=3 where IdFilas=' + cont2 + ' and C' + con2 + ' IN(0);');
+            cont = cont2;
+            console.log('entra' + con2 + ' Valor en fila' + cont2);
+
+        }
+    };
+
+
+
+
     // hacer la consulta 
     function EnviarDatos() {
         socket.emit('Cargar Datos', function (resultado) {
@@ -58,6 +88,10 @@ io.on('connection', function (socket) {
     setInterval(() => {
         EnviarDatos();
     }, 1000);
+
+    setInterval(() => {
+        GenerarObstaculos();
+    }, 3000);
 });
 
 
